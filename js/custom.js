da2db8fbf1072bbf59cf58a65ceb8444210b2939
Claudia Windberger <Claudia@Claudias-MacBook-Air.local>
2017-02-22 23:07:55 +0000

    update feb

--- js/custom.js
+++ js/custom.js
@@ -6,19 +6,50 @@ $(document).ready(function() {
 	$('.arrow').click(function(){
     $("html, body").animate({ scrollTop: $(window).height()}, 600);
     return false;
+	});
+	
+	$(".priceTag button").hover(function(){
+		$(".fa.fa-shopping-basket").toggleClass("black");
+		});
+
+var time_id;
+
+	$(".img1, .img2, .img3").hover(
+	
+	function(e){
+		e.preventDefault();
+    	var hovered = $(this);
+			if (time_id) {
+			clearTimeout(time_id);
+			} 
+			time_id = setTimeout(function () {
+				hovered.find("div:first").stop(true, true).slideDown('slow');
+			}, 500);
+	}, 
+	
+	function (e) {
+		e.preventDefault();
+    	var hovered = $(this);
+			if (time_id) {
+			clearTimeout(time_id);
+    		} 
+			time_id = setTimeout(function () {
+				hovered.find("div:first").stop(true, true).slideUp('slow');
+			}, 200);
+	});
+	
+
+ 	$(function() {
+   		var availableTags = [{label:"Basic", the_link:"http://www.msn.com"},{label:"C++", the_link:"http://www.ibm.com"},{label:"Fortran", the_link:"http://www.yahoo.com"}];
+    	$( "#tags" ).autocomplete({
+      	source: availableTags,
+      	select:function(e,ui) { 
+      	location.href = ui.item.the_link;
+    //  console.log(ui.item.the_link);
+      }
+    });
 });
 
-  $( function() {
-    var availableTags = [
-      "Cake1",
-      "Cake2",
-      "cake3"
-    ];
-    $( "#tags" ).autocomplete({
-      source: availableTags
-    });
-  } );
-  
   
 });
 
